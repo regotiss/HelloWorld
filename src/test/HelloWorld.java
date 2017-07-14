6c041a089ffd5090b6d60b0de9ca8d8aad17f30c
surajtar <surajtar@in-blr-surajtar.local>
2017-07-14 17:41:07 +0530

    first commit

---
+++ src/test/HelloWorld.java
@@ -0,0 +1,11 @@
+package test;
+
+/**
+ * Created by surajtar on 7/14/17.
+ */
+public class HelloWorld {
+
+    public static void main(String[] args){
+        System.out.println("Hello World");
+    }
+}
